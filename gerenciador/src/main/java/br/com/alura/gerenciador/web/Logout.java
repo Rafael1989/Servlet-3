7628cae98de855cb115b9d67f3e9cacdbb049267
Rafael <Rafael@10.20.172.194>
2017-12-19 14:12:14 -0200

    Redirecionamento

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -3,6 +3,7 @@ package br.com.alura.gerenciador.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -25,9 +26,11 @@ public class Logout extends HttpServlet{
 		HttpSession session = req.getSession();
 		
 		session.removeAttribute("usuario.logado");
-		
-		PrintWriter writer = resp.getWriter();
-		writer.println("<html><body>Logout efetuado</body></html>");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logout.html");
+		dispatcher.forward(req, resp);
+		//resp.sendRedirect("logout.html"); redirecionamento feito do lado do client 302
+		/*PrintWriter writer = resp.getWriter();
+		writer.println("<html><body>Logout efetuado</body></html>");*/
 	}
 
 }
